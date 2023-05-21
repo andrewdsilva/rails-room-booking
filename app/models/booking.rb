ebf8148ffcb7166b1007907718ef052ea3e3c3ed
Nathan Lopez <nathan.lopez042@gmail.com>
2023-05-21 14:54:44 +0200

    Add more tests and model validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,15 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
+  validates :start_date, :end_date, :room_id, :user_id, presence: true
+  validate :end_date_greater_than_start_date
+
+  def end_date_greater_than_start_date
+    if end_date && start_date && end_date <= start_date
+      errors.add(:end_date, "must be greater than start date")
+    end
+  end
+
   def self.default_sort
     "created_at desc"
   end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -14,4 +14,23 @@ RSpec.describe ::Booking, type: :model do
 
     expect(booking.total_price).to eq(450)
   end
+
+  it "Should require start and end dates" do
+    user = create(:user_1)
+    room = create(:room_1, user: user)
+
+    booking = build(:booking_1, user: user, room: room, start_date: nil, end_date: nil)
+
+    expect(booking).not_to be_valid
+    expect(booking.errors[:start_date]).to include("ne peut pas être vide")
+    expect(booking.errors[:end_date]).to include("ne peut pas être vide")
+
+    start_date = DateTime.new(2023, 5, 21)
+    end_date   = DateTime.new(2023, 5, 20) # End date is before start date
+
+    booking = build(:booking_1, user: user, room: room, start_date: start_date, end_date: end_date)
+
+    expect(booking).not_to be_valid
+    expect(booking.errors[:end_date]).to include("must be greater than start date")
+  end
 end
