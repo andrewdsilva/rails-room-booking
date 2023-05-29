9b8394481dcf11709a2e5151050cf61d1d143cce
Nathan Lopez <nathan.lopez042@gmail.com>
2023-05-29 21:37:46 +0200

    Room list view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def currency_str(ammount)
+    return "" if ammount.nil?
+
     format = I18n.t :format, scope: "number.currency.format"
     Money.from_amount(ammount, "EUR").format(format: format)
   end
