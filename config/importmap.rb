24f41b551d90d65ec47702de8b649883c752366e
Nathan Lopez <nathan.lopez042@gmail.com>
2023-06-03 14:16:59 +0200

    Add drag and drop for image upload in room form

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,13 @@
 pin 'application', preload: true
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin "@rails/activestorage", to: "activestorage.esm.js"
+
+pin 'dropzone', to: 'https://ga.jspm.io/npm:dropzone@6.0.0-beta.2/dist/dropzone.mjs'
+pin 'just-extend', to: "https://ga.jspm.io/npm:just-extend@5.1.1/index.esm.js" # required by dropzone
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
