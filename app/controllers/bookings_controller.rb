20b7a235939313c9e3afe0bc593170849d2a8601
Nathan Lopez <nathan.lopez042@gmail.com>
2023-06-05 21:01:54 +0200

    Create booking form and controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,23 @@
+class BookingsController < ApplicationController
+  include ::CrudConcern
+
+  private
+
+  def base_class
+    ::Booking
+  end
+
+  def object_class
+    base_class.all
+  end
+
+  def strong_params
+    [
+      :start_date, :end_date, :num_guests, :room_id
+    ]
+  end
+
+  def model_params
+    :booking
+  end
+end
