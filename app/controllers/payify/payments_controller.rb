6425630609e66fdec5819b57d45fff4acc2c97ec
Nathan Lopez <nathan.lopez042@gmail.com>
2023-06-22 20:46:15 +0200

    Update payment form and confirmation page

--- app/controllers/payify/payments_controller.rb
+++ app/controllers/payify/payments_controller.rb
@@ -1,5 +1,14 @@
 module Payify
   class PaymentsController < ApplicationController
     include ::Payify::PaymentsControllerConcern
+
+    before_action :set_booking, only: [:complete]
+
+    private
+
+    def set_booking
+      @booking = @payment.model
+    end
+
   end
 end
